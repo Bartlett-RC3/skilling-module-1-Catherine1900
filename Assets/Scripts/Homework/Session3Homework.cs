2184f77f3c1cfe525136c1420a6fc5eace5ff205
kanshiyu <kanshiyu@kanshiyudeMacBook-Pro.local>
2018-10-27 16:31:17 +0100

    Session3_Homework

---
+++ Assets/Scripts/Homework/Session3Homework.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session3Homework : MonoBehaviour
+{
+
+    public GameObject CubePrefab;
+    Renderer m_Renderer;
+    Renderer child_renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Instantiate(CubePrefab, this.transform);
+        m_Renderer = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //    * Create a cube and move it across the screen
+        foreach (Transform child in this.transform)
+        {
+            child.Translate(Time.deltaTime, 0, 0);
+        }
+
+        //     * Create a cube and change its colour when key is pressed or mouse is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_Renderer.material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Renderer.material.color = new Color(Random.value, Random.value, Random.value);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/School/Session3_Kan.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session3_Kan : MonoBehaviour {
+    public GameObject cubeprefab;
+    public GameObject Light;
+    public GameObject objectReference;
+    public int spacing = 10;
+    public int gridX = 10;
+    public int gridY = 10;
+
+    int var = 10;
+
+    // Always here. 
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Instantiate(cubeprefab, new Vector3 (i*spacing , j*spacing,0), Quaternion.identity,this.transform);
+            }
+        }
+    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Time.realtimeSinceStartup
+        // Time.deltaTime
+        // Time.time
+        Debug.Log("This computer can redner a frame in: " + Time.deltaTime);
+        Debug.Log("Since I started playing the game this amount of time has passed: " + Time.timeSinceLevelLoad);
+        Debug.Log("omputer has counted this ammount of frames: " + Time.frameCount);
+
+
+        foreach(Transform child in this.transform)
+        {
+            child.Translate(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+        }
+
+        // rotatiom
+        // rotate children gameobject around y axis
+
+        foreach (Transform child in this.transform)
+        {
+            child.RotateAroundLocal(Vector3.up, Random.value);
+        }
+
+        foreach (Transform child in this.transform)
+        {
+            child.localScale = new Vector3(Random.Range(0.1f, 1), Random.Range(0.1f, 1), Random.Range(0.1f, 1));
+        }
+
+
+        // Record the cubes orginal scale
+        Vector3[] originalScale = new Vector3[this.transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            originalScale[i] = transform.GetChild(i).localScale;
+        }
+
+
+        // keyboard input
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // if i press space
+            // if i re
+         
+            foreach (Transform child in this.transform)
+            {
+
+                child.localScale = child.localScale * 5f;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).localScale = originalScale[i];
+            }
+        }
+
+        // MOUSE PRESSED (O left, 1 right, 2 middle)
+        if (Input.GetMouseButton(0))
+        {
+            Light.GetComponent<Light>().color = new Color(Random.value, Random.value, Random.value);
+        }
+
+
+    }
+}
